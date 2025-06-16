c4007bf4b766de8e277a199c451f65305311ce74
AzadAb <azadmda934@gmail.com>
2025-06-16 17:36:03 +0530

    day2

--- arrays.js
+++ arrays.js
@@ -63,13 +63,77 @@
 
 
 
-// Reduce()
+// // Reduce()
 
-let numbers = [1, 4, 9, 16];
-let result = numbers.reduce((sum, x) => sum + x);
-console.log(result);
+// let numbers = [1, 4, 9, 16];
+// let result = numbers.reduce((sum, x) => sum + x);
+// console.log(result);
 
 
-// some and every and find and findIndex() and sort()
+// // some and every and find and findIndex() and sort()
 
-let number = [1, 3, 4];
+// let number = [1, 3, 4];
+
+
+// Map()
+
+// let numbers = [1, 4, 9, 16];
+
+// const result = numbers.filter((e) => {
+//     return e > 10;
+// });
+
+
+// console.log(result)
+
+
+
+// let numbers = [1, 2, 3, 4];
+// const result = numbers.reduce((sum, e) => {
+//     return sum + e;
+// })
+// console.log(result)
+
+
+
+// const cart = [
+//     { item: "book", price: 120 },
+//     { item: "pen", price: 20 },
+//     { item: "NoteBook", price: 60 }
+// ];
+
+// const total = cart.reduce((sum, product) => {
+//     return sum + product.price;
+// }, 0)
+
+// console.log(total);
+
+
+// some()
+
+// let score = [85, 72, 90, 67];
+
+// let result = score.some((x) => x > 100);
+// console.log(result)
+
+
+// let students = [
+//     { name: "Azad", marks: 85 },
+//     { name: "Ravi", marks: 35 },
+//     { name: "Meena", marks: 45 }
+// ];
+// const isfialed = students.some((e) => {
+//     return e.marks < 40;
+// })
+// if (isfialed) {
+//     console.log("the student is falied Because: ");
+//     for (let s of students) {
+//         if (s.marks < 40) {
+//             console.log(` student got only marks of ${s.marks} It is less than pass mark of ${40 - s.marks}`);
+//         }
+//     }
+// }
+
+// let a = [10, 40, 260, 12];
+// a.sort((a, b) => b - a);
+// console.log(a)
